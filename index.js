afe0720f560677d7e2d3cbb02695492bbe5d85d8
burawi <burawi.ba@gmail.com>
2016-12-12 13:55:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+var fs = require('fs');
+var lex = require('pug-lexer');
+var parse = require('pug-parser');
+var generateCode = require('pug-code-gen');
+var wrap = require('pug-runtime/wrap');
+var load = require('pug-load');
+var link = require('pug-linker');
+var path = require('path');
+
+var getAst = function (filename) {
+    var str = fs.readFileSync(filename, 'utf-8');
+    var tokens = lex(str);
+    var ast = parse(tokens,{filename: filename, src: str});
+    var ast = load(ast, {
+        lex: lex,
+        parse: parse,
+        resolve: function(filename, source, options) {
+            console.log('"' + filename + '" file requested from "' + source + '".');
+            return load.resolve(filename, source, options);
+        }
+    });
+    return ast;
+}
+
+var Layout = function (layoutFilename) {
+    this.data = {
+        type: "Extends",
+        file: {
+            ast: getAst(layoutFilename)
+        }
+    };
+}
+
+Layout.prototype.includeAtTop = function (layoutFilename) {
+    var file = {
+        type: "Include",
+        file: {
+            type: "FileReference",
+            ast: getAst(layoutFilename)
+        },
+        block: {
+            type: "Block",
+            nodes: []
+        }
+    };
+    this.data.file.ast.nodes.splice(0,0,file);
+}
+
+Layout.prototype.includeMixin = function (filename,args,name) {
+    if(filename !== undefined){
+        if(name === undefined){
+            name = path.basename(filename,'.pug');
+        }
+        if(args === undefined){
+            args = "";
+        }
+        var mixin = {
+            type: "Mixin",
+            name: name,
+            args: args,
+            block: getAst(filename)
+        };
+        this.data.file.ast.nodes.splice(0,0,mixin);
+    }
+}
+
+Layout.prototype.render = function (childFilename) {
+    var layout = this.data;
+    var childAst = getAst(childFilename);
+    childAst.nodes.splice(0,0,layout);
+    childAst = link(childAst);
+
+    var funcStr = generateCode(childAst, {
+        compileDebug: false,
+        pretty: true,
+        inlineRuntimeFunctions: false,
+        templateName: 'helloWorld'
+    });
+
+    var func = wrap(funcStr, 'helloWorld');
+    return func();
+}
+
+Layout.prototype.renderInFile = function(src, dist){
+    var html = this.render(src);
+    fs.writeFileSync(dist, html, 'utf-8');
+}
+
+module.exports = Layout;
---
+++ readme.md
@@ -0,0 +1,133 @@
+# pug-layout
+A module that gives you the possibility of loading a layout in variable
+
+## Install
+
+```
+npm install pug-layout
+
+```
+
+## Usage
+
+**layout.pug:**
+
+```pug
+div(foo="bar")
+    block content
+```
+
+**index.pug:**
+
+```pug
+block content
+    span this is the content
+```
+
+**app.js:**
+
+```javascript
+var Layout = require('pug-layout');
+
+var layout = new Layout('layout.pug');
+
+layout.renderInFile('index.pug', 'index.html');
+```
+
+And you will get this **index.html:**
+
+```html
+<div foo="bar">
+    <span>this is the content</span>
+</div>
+```
+
+## Load Mixin
+You can load a file as a mixin, that means the content of the file can be used as mixin:
+
+`.includeMixin(filePath,mixinArgs,mixinName)`:
+
+- *mixinArgs*: string containing arguments of the mixin, if multiple separate them with comma `,`
+- *mixinName*: the name of mixin (optional), if not provided the mixin will be named with its filename.
+
+Ex:
+
+**pet.pug:**
+```pug
+p
+    div= type
+    div= age
+```
+
+**index.pug:**
+```pug
+block content
+    span this is the content
+    +pet('dog',5)
+```
+
+**app.js:**
+```javascript
+var Layout = require('pug-layout');
+
+var layout = new Layout('layout.pug');
+
+layout.includeMixin('pet.pug','type,age');
+
+layout.renderInFile('index.pug', 'index.html');
+```
+
+And now you will get **index.html:**
+```html
+
+<div foo="bar"><span>this is the content</span>
+  <p>
+    <div>dog</div>
+    <div>5</div>
+  </p>
+</div>
+```
+
+## Include at Top
+You can include a file at top of layout. *Useful when you want to include multiple mixins from one file.*
+
+`.includeAtTop(filePath)`:
+
+Ex:
+
+**head.pug:**
+```pug
+section.head I'm the header
+```
+
+**app.js:**
+```javascript
+var Layout = require('pug-layout');
+
+var layout = new Layout('layout.pug');
+
+layout.includeMixin('pet.pug','type,age');
+layout.includeAtTop('head.pug');
+
+layout.renderInFile('index.pug', 'index.html');
+```
+
+And now you will get **index.html:**
+```html
+
+<section class="head">I'm the header</section>
+<div foo="bar"><span>this is the content</span>
+  <p>
+    <div>dog</div>
+    <div>5</div>
+  </p>
+</div>
+```
+
+## Get HTML
+You can get the rendered HTML without writing it in a file, using `render(filePath)` instead of `renderInFile(filePath,dist)`.
+
+Ex:
+```javascript
+var html = layout.render('index.pug');
+```
